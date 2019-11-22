1c08eb184a9668eab04cd48da001da8feb3b1798
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2019-11-22 23:43:55 +0700

    Revert "แก้ Design"
    This reverts commit 7f7c51d37a7555e17506a4edbd791fd61a1dd134.

--- SwingInspire/src/swing/Login.java
+++ SwingInspire/src/swing/Login.java
@@ -190,7 +190,7 @@ public class Login extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Window".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }

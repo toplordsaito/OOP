b795bb37d2c3893ae59d1d2c7e00ec356f358222
Ason <aloha_sonza@hotmail.com>
2019-11-22 23:27:02 +0700

    Update Login.java

--- SwingInspire/src/swing/Login.java
+++ SwingInspire/src/swing/Login.java
@@ -189,8 +189,8 @@ public class Login extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Window".equals(info.getName())) {
+             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
